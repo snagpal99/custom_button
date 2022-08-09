1ad59295d22b51bff3198648a602cd5aa6f09964
Shreyansh Nagpal <snagpal@microsoft.com>
2022-08-10 02:03:12 +0530

    Add fluent button

---
+++ src/components/FluentButton/FluentButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { PrimaryButton } from '@fluentui/react/lib/Button';
+
+const FluentButton = () => {
+  return <PrimaryButton>Fluent Button</PrimaryButton>
+};
+
+// class FluentButton extends React.Component {
+//   render() {
+//     return <PrimaryButton>Fluent Button</PrimaryButton>
+//   }
+// }
+
+export default FluentButton;
